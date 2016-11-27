4b43ba1ac504613bd6b539f1be01209351b8c2b9
Matt Casey <matt@mattcasey.nyc>
2016-11-27 16:42:06 -0500

    moved to typescript (build & test broken)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component<{}, {}> {
+  public render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
