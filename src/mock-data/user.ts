764f68e15f67abe965810d81f1f4accd3ff61a4c
Matt Casey <matt@mattcasey.nyc>
2016-11-30 02:15:11 -0500

    new mock user data

---
+++ src/mock-data/user.ts
@@ -0,0 +1,16 @@
+import { user } from '../modules';
+
+const mockUser: user.State = {
+  id: 0,
+  name: 'Joe Plumber',
+  location: {
+    name: '143 Mott St, New York, NY 10013',
+    state: 'NY',
+    district: 2,
+    latitude: 1234,
+    longitude: 1234,
+  },
+  causes: [],
+};
+
+export default mockUser;
