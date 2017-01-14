4d85219a02661d5f23b6f0708e2a167b15725f49
Matt Casey <matt@mattcasey.nyc>
2017-01-14 14:06:45 -0500

    fixed serving of static content

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ const path = require('path');
 const app = express();
 
 const port = (process.env.PORT || 8080);
+const root = path.join(__dirname, '../build');
 
-app.use(express.static('../build'));
-app.use('/static', express.static('../build'));
+app.use(express.static(root));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(root, 'index.html'));
 });
 
-app.listen(port);
+const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
